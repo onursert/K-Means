574016ad917d87296e55a6ba9616fd34761b6f64
Onur <43381986+onursert@users.noreply.github.com>
2019-12-14 19:33:55 +0300

    Add files via upload

---
+++ KMeans/KMeans.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace KMeans
+{
+    partial class KMeans
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownC = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownK = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownK)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.numericUpDownC);
+            this.panel1.Controls.Add(this.numericUpDownK);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.buttonStart);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1008, 562);
+            this.panel1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 540);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "github.com/onursert";
+            // 
+            // numericUpDownC
+            // 
+            this.numericUpDownC.Location = new System.Drawing.Point(779, 530);
+            this.numericUpDownC.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDownC.Name = "numericUpDownC";
+            this.numericUpDownC.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDownC.TabIndex = 22;
+            // 
+            // numericUpDownK
+            // 
+            this.numericUpDownK.Location = new System.Drawing.Point(656, 530);
+            this.numericUpDownK.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownK.Name = "numericUpDownK";
+            this.numericUpDownK.Size = new System.Drawing.Size(50, 20);
+            this.numericUpDownK.TabIndex = 21;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(712, 532);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Item Count:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(595, 532);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "K (Max 5):";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(885, 527);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(112, 23);
+            this.buttonStart.TabIndex = 11;
+            this.buttonStart.Text = "Start Simulation";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1007, 521);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 562);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "K-Means";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownK)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDownC;
+        private System.Windows.Forms.NumericUpDown numericUpDownK;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ KMeans/KMeans.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KMeans
+{
+    public partial class KMeans : Form
+    {
+        Graphics graphics;
+        Pen pen;
+        Point point;
+
+        public KMeans()
+        {
+            InitializeComponent();
+
+            graphics = pictureBox1.CreateGraphics();
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            pen = new Pen(Color.Black, 3);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Drawing coordinate system.
+            Image image = new Bitmap(1040, 1040);
+            pictureBox1.Image = image;
+            Graphics graphics = Graphics.FromImage(pictureBox1.Image);
+            Font font = new Font("Calibri", 10);
+            graphics.Transform = new Matrix();
+            Matrix matrix = new Matrix();
+            //Vertical
+            for (int i = 0; i <= 1000; i += 50)
+            {
+                graphics.DrawString((i / 10).ToString(), font, Brushes.Red, 5, (i - 10));
+
+                graphics.DrawLine(pen, -5, i, 5, i);
+                graphics.DrawLine(pen, i, -5, i, 5);
+            }
+            //Horizontal
+            matrix.Rotate(90, MatrixOrder.Prepend);
+            graphics.Transform = matrix;
+            for (int i = 0; i <= 1000; i += 50)
+            {
+                graphics.DrawString((i / 10).ToString(), font, Brushes.Red, 5, (-i - 10));
+            }
+        }
+
+        Random random = new Random();
+
+        int[] redXY = new[] { 0, 0 };
+        int[] greenXY = new[] { 0, 0 };
+        int[] blueXY = new[] { 0, 0 };
+        int[] yellowXY = new[] { 0, 0 };
+        int[] purpleXY = new[] { 0, 0 };
+
+        int[] redXYForCheck = new[] { 0, 0 };
+        int[] greenXYForCheck = new[] { 0, 0 };
+        int[] blueXYForCheck = new[] { 0, 0 };
+        int[] yellowXYForCheck = new[] { 0, 0 };
+        int[] purpleXYForCheck = new[] { 0, 0 };
+
+        List<int[]> item = new List<int[]>();
+
+        List<int[]> redPoint = new List<int[]>();
+        List<int[]> greenPoint = new List<int[]>();
+        List<int[]> bluePoint = new List<int[]>();
+        List<int[]> yellowPoint = new List<int[]>();
+        List<int[]> purplePoint = new List<int[]>();
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+
+            redXY = new[] { 0, 0 };
+            greenXY = new[] { 0, 0 };
+            blueXY = new[] { 0, 0 };
+            yellowXY = new[] { 0, 0 };
+            purpleXY = new[] { 0, 0 };
+
+            redXYForCheck = new[] { 0, 0 };
+            greenXYForCheck = new[] { 0, 0 };
+            blueXYForCheck = new[] { 0, 0 };
+            yellowXYForCheck = new[] { 0, 0 };
+            purpleXYForCheck = new[] { 0, 0 };
+
+            redPoint = new List<int[]>();
+            greenPoint = new List<int[]>();
+            bluePoint = new List<int[]>();
+            yellowPoint = new List<int[]>();
+            purplePoint = new List<int[]>();
+
+            step1();
+
+            Thread.Sleep(2000);
+
+            step2();
+
+            Thread.Sleep(2000);
+            pictureBox1.Refresh();
+
+            step3();
+
+            while (!checkIfSameList())
+            {
+                Thread.Sleep(300);
+                pictureBox1.Refresh();
+
+                step3();
+            }
+        }
+        public bool checkIfSameList()
+        {
+            var redBool = redXY.Except(redXYForCheck).ToList();
+            var greenBool = greenXY.Except(greenXYForCheck).ToList();
+            var blueBool = blueXY.Except(blueXYForCheck).ToList();
+            var yellowBool = yellowXY.Except(yellowXYForCheck).ToList();
+            var purpleBool = purpleXY.Except(purpleXYForCheck).ToList();
+
+            return !redBool.Any() && !greenBool.Any() && !blueBool.Any() && !yellowBool.Any() && !purpleBool.Any();
+        }
+
+        public void step1()
+        {
+            //Step 1
+            //Drawing items.
+            item.Clear();
+            for (int i = 0; i < Convert.ToInt16(numericUpDownC.Value); i++)
+            {
+                int[] coordinates = new int[] { 0, 0 };
+                coordinates[0] = random.Next(0, 1000);
+                coordinates[1] = random.Next(0, 500);
+
+                graphics.DrawEllipse(new Pen(Brushes.Black, 3), coordinates[0], coordinates[1], 5, 5);
+                item.Add(coordinates);
+            }
+        }
+        public void step2()
+        {
+            //Step 2
+            //Drawing random k means points.
+            for (int k = 0; k < Convert.ToInt16(numericUpDownK.Value); k++)
+            {
+                if (k == 0)
+                {
+                    redXY[0] = random.Next(0, 1000);
+                    redXY[1] = random.Next(0, 500);
+
+                    graphics.DrawEllipse(new Pen(Brushes.DarkRed, 3), redXY[0], redXY[1], 20, 20);
+                }
+                else if (k == 1)
+                {
+                    greenXY[0] = random.Next(0, 1000);
+                    greenXY[1] = random.Next(0, 500);
+
+                    graphics.DrawEllipse(new Pen(Brushes.DarkGreen, 3), greenXY[0], greenXY[1], 20, 20);
+                }
+                else if (k == 2)
+                {
+                    blueXY[0] = random.Next(0, 1000);
+                    blueXY[1] = random.Next(0, 500);
+
+                    graphics.DrawEllipse(new Pen(Brushes.DarkBlue, 3), blueXY[0], blueXY[1], 20, 20);
+                }
+                else if (k == 3)
+                {
+                    yellowXY[0] = random.Next(0, 1000);
+                    yellowXY[1] = random.Next(0, 500);
+
+                    graphics.DrawEllipse(new Pen(Brushes.Gold, 3), yellowXY[0], yellowXY[1], 20, 20);
+                }
+                else if (k == 4)
+                {
+                    purpleXY[0] = random.Next(0, 1000);
+                    purpleXY[1] = random.Next(0, 500);
+
+                    graphics.DrawEllipse(new Pen(Brushes.Indigo, 3), purpleXY[0], purpleXY[1], 20, 20);
+                }
+            }
+        }
+        public void step3()
+        {
+            redXYForCheck = redXY;
+            greenXYForCheck = greenXY;
+            blueXYForCheck = blueXY;
+            yellowXYForCheck = yellowXY;
+            purpleXYForCheck = purpleXY;
+
+            redPoint = new List<int[]>();
+            greenPoint = new List<int[]>();
+            bluePoint = new List<int[]>();
+            yellowPoint = new List<int[]>();
+            purplePoint = new List<int[]>();
+
+            //For every item, calculate distance between items and random k points. Find min. distance and draw again.
+            for (int i = 0; i < item.Count; i++)
+            {
+                List<double> distance = new List<double>();
+
+                for (int k = 0; k < Convert.ToInt16(numericUpDownK.Value); k++)
+                {
+                    if (k == 0)
+                    {
+                        distance.Add(calculateDistance(item[i][0], item[i][1], redXY[0], redXY[1]));
+                    }
+                    else if (k == 1)
+                    {
+                        distance.Add(calculateDistance(item[i][0], item[i][1], greenXY[0], greenXY[1]));
+                    }
+                    else if (k == 2)
+                    {
+                        distance.Add(calculateDistance(item[i][0], item[i][1], blueXY[0], blueXY[1]));
+                    }
+                    else if (k == 3)
+                    {
+                        distance.Add(calculateDistance(item[i][0], item[i][1], yellowXY[0], yellowXY[1]));
+                    }
+                    else if (k == 4)
+                    {
+                        distance.Add(calculateDistance(item[i][0], item[i][1], purpleXY[0], purpleXY[1]));
+                    }
+                }
+
+                if (distance.Count > 0 && distance.Min() == distance[0])
+                {
+                    graphics.DrawEllipse(new Pen(Brushes.Red, 3), item[i][0], item[i][1], 5, 5);
+                    redPoint.Add(item[i]);
+                }
+                else if (distance.Count > 1 && distance.Min() == distance[1])
+                {
+                    graphics.DrawEllipse(new Pen(Brushes.Green, 3), item[i][0], item[i][1], 5, 5);
+                    greenPoint.Add(item[i]);
+                }
+                else if (distance.Count > 2 && distance.Min() == distance[2])
+                {
+                    graphics.DrawEllipse(new Pen(Brushes.Blue, 3), item[i][0], item[i][1], 5, 5);
+                    bluePoint.Add(item[i]);
+                }
+                else if (distance.Count > 3 && distance.Min() == distance[3])
+                {
+                    graphics.DrawEllipse(new Pen(Brushes.Yellow, 3), item[i][0], item[i][1], 5, 5);
+                    yellowPoint.Add(item[i]);
+                }
+                else if (distance.Count > 4 && distance.Min() == distance[4])
+                {
+                    graphics.DrawEllipse(new Pen(Brushes.Purple, 3), item[i][0], item[i][1], 5, 5);
+                    purplePoint.Add(item[i]);
+                }
+            }
+
+            for (int k = 0; k < Convert.ToInt16(numericUpDownK.Value); k++)
+            {
+                if (k == 0 && redPoint.Count > 0)
+                {
+                    int x = 0;
+                    int y = 0;
+                    for (int i = 0; i < redPoint.Count; i++)
+                    {
+                        x += redPoint[i][0];
+                        y += redPoint[i][1];
+                    }
+                    x = x / redPoint.Count;
+                    y = y / redPoint.Count;
+                    graphics.DrawEllipse(new Pen(Brushes.DarkRed, 3), x, y, 20, 20);
+
+                    redXY = new[] { x, y };
+                }
+                if (k == 1 && greenPoint.Count > 0)
+                {
+                    int x = 0;
+                    int y = 0;
+                    for (int i = 0; i < greenPoint.Count; i++)
+                    {
+                        x += greenPoint[i][0];
+                        y += greenPoint[i][1];
+                    }
+                    x = x / greenPoint.Count;
+                    y = y / greenPoint.Count;
+                    graphics.DrawEllipse(new Pen(Brushes.DarkGreen, 3), x, y, 20, 20);
+
+                    greenXY = new[] { x, y };
+                }
+                else if (k == 2 && bluePoint.Count > 0)
+                {
+                    int x = 0;
+                    int y = 0;
+                    for (int i = 0; i < bluePoint.Count; i++)
+                    {
+                        x += bluePoint[i][0];
+                        y += bluePoint[i][1];
+                    }
+                    x = x / bluePoint.Count;
+                    y = y / bluePoint.Count;
+                    graphics.DrawEllipse(new Pen(Brushes.DarkBlue, 3), x, y, 20, 20);
+
+                    blueXY = new[] { x, y };
+                }
+                else if (k == 3 && yellowPoint.Count > 0)
+                {
+                    int x = 0;
+                    int y = 0;
+                    for (int i = 0; i < yellowPoint.Count; i++)
+                    {
+                        x += yellowPoint[i][0];
+                        y += yellowPoint[i][1];
+                    }
+                    x = x / yellowPoint.Count;
+                    y = y / yellowPoint.Count;
+                    graphics.DrawEllipse(new Pen(Brushes.Gold, 3), x, y, 20, 20);
+
+                    yellowXY = new[] { x, y };
+                }
+                else if (k == 4 && purplePoint.Count > 0)
+                {
+                    int x = 0;
+                    int y = 0;
+                    for (int i = 0; i < purplePoint.Count; i++)
+                    {
+                        x += purplePoint[i][0];
+                        y += purplePoint[i][1];
+                    }
+                    x = x / purplePoint.Count;
+                    y = y / purplePoint.Count;
+                    graphics.DrawEllipse(new Pen(Brushes.Indigo, 3), x, y, 20, 20);
+
+                    purpleXY = new[] { x, y };
+                }
+            }
+        }
+        public double calculateDistance(int itemX, int itemY, int kPointX, int kPointY)
+        {
+            double x2 = Math.Pow(itemX - kPointX, 2);
+            double y2 = Math.Pow(itemY - kPointY, 2);
+            double squareRoot = Math.Sqrt(x2 + y2);
+            return squareRoot;
+        }
+    }
+}
